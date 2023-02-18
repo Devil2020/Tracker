5eeb9d7973efb69fe80f7372bce1178ddf985983
mohamed.morse <mohamed.morse@expertapps.com.sa>
2023-02-18 18:01:06 +0200

    make status bar transparent

--- app/src/main/java/com/morse/trackizer/ui/launcher/MainActivity.kt
+++ app/src/main/java/com/morse/trackizer/ui/launcher/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.morse.trackizer.ui.launcher
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,29 +16,20 @@ import com.morse.trackizer.ui.theme.TrackizerTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        hideStatusBarAndExpandScreen()
         setContent {
             TrackizerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TrackizerTheme {
-        Greeting("Android")
+    private fun hideStatusBarAndExpandScreen() {
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_LOW_PROFILE)
     }
 }
