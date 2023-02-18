f0b647c926652b4387a9b37ad0c2ee9ecc7f72f7
mohamed.morse <mohamed.morse@expertapps.com.sa>
2023-02-18 17:05:25 +0200

    Initialize Classes that i needed them for the project

---
+++ app/src/main/java/com/morse/trackizer/app/TrackizerApplication.kt
@@ -0,0 +1,7 @@
+package com.morse.trackizer.app
+
+import android.app.Application
+
+class TrackizerApplication : Application() {
+
+}
---
+++ app/src/main/java/com/morse/trackizer/ui/screens/subscription/SubscriptionDetailsScreen.kt
@@ -0,0 +1,2 @@
+package com.morse.trackizer.ui.screens.subscription
+
